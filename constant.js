ed6e04422ab310900ac5594465fa558d6a70973c
Manuelmoranrod <73696795+Manuelmoranrod@users.noreply.github.com>
2023-05-15 17:56:58 +0200

    Delete constant.js

--- constant.js
+++
@@ -1,8 +0,0 @@
-export const DB_USER = "admin"
-export const DB_PASSWORD = "admin12345"
-export const DB_HOST = "chatapp.s41jees.mongodb.net"
-
-
-export const IP_SERVER = "localhost";
-
-export const PORT = process.env.PORT || 3977
